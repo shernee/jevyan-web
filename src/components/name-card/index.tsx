1b308cd72c11216c95e43e320da429f0848fd902
Neetha Sherra <shernee93@gmail.com>
2020-09-07 08:04:30 +0000

    name-card component and styling

---
+++ src/components/name-card/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.css'
+
+export default function NameCard() {
+  return (
+    <section id="name-card-section">
+      <div className="container">
+        <h1 className="restaurant-name">
+          Next Door Kitchen
+        </h1>
+        <h4 className="contact-info">
+          Contact 9731076551
+        </h4>
+      </div>
+    </section>
+  )
+}
