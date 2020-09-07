e557d2e93b70b09bd0f1bd860aa3d1ccd03f0804
Neetha Sherra <shernee93@gmail.com>
2020-09-07 08:05:29 +0000

    added banner to menu page

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
+import Banner from '../../components/banner'
 
 export default function Menu() {
   return (
-    <h1>
-      Jevyan Menu
-    </h1>
+    <Banner />
   )
 }
