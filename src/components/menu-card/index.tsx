34fe5e9d8df4d516dd5643450654b14003f8ff38
Neetha Sherra <shernee93@gmail.com>
2020-09-08 01:08:16 +0000

    added menu card component

---
+++ src/components/menu-card/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './index.css'
+
+export default function MenuCard() {
+  const items: { [category:string]: Array<string> } = {}
+  items.Regular = ['Chicken Cutlets', 'Meat Pepper Sukka', 'Pork Chilly/Bafat', 'Sanna']
+  items.Specials = ['Patholyo', 'Mackerel Fry']
+
+  return (
+    <section id="menu-card">
+      <div className="container">
+        {Object.keys(items).map((category, catIndex) => (
+          <div id={category} className="category-rows" key={catIndex.toString()}>
+            <h2>{category}</h2>
+            <div className="item-card-box">
+              {items[category].map((item, itemIndex) => (
+                <div className="item-card" key={itemIndex.toString()}>
+                  <div className="item-details">
+                    <h6>{item}</h6>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  )
+}
