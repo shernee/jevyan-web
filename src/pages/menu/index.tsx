4917cad45e6acbf60ff49892310609b13e66e031
Neetha Sherra <shernee93@gmail.com>
2020-09-08 01:09:12 +0000

    added changed components to menu page

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import Banner from '../../components/banner'
+import CategoryHeader from '../../components/category-header'
+import MenuCard from '../../components/menu-card'
 
 export default function Menu() {
   return (
-    <Banner />
+    <div className="menu-page">
+      <Banner />
+      <CategoryHeader />
+      <MenuCard />
+    </div>
   )
 }
