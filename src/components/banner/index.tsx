c6e7f655cf850808ab2c58028a8e8c037072a63c
Neetha Sherra <shernee93@gmail.com>
2020-09-09 00:01:39 +0000

    added responsiveness to banner

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import './index.css'
-import NameCard from '../name-card/index'
+import NameCard from '../name-card'
 
 export default function Banner() {
   return (
     <section id="main-banner-section">
-      <div id="main-banner">
-        <div className="white-card">
-          <NameCard />
-        </div>
+      <div className="white-card">
+        <NameCard />
       </div>
     </section>
   )
