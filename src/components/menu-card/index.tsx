d0e661f299b6ca58fba1072b84af3543e0547c42
Neetha Sherra <shernee93@gmail.com>
2020-09-09 07:48:03 +0000

    added content within the item card

--- src/components/menu-card/index.tsx
+++ src/components/menu-card/index.tsx
@@ -1,23 +1,33 @@
 import React from 'react'
 import './index.css'
+import ItemDetails from './items.js'
 
 export default function MenuCard() {
-  const items: { [category:string]: Array<string> } = {}
-  items.Regular = ['Chicken Cutlets', 'Meat Pepper Sukka', 'Pork Chilly/Bafat', 'Sanna']
-  items.Specials = ['Patholyo', 'Mackerel Fry']
+  interface IItem {
+    name: string, desc: string, price: string
+  }
+
+  const items: { [categoryName:string]: Array<IItem> } = ItemDetails
+  const categories: Array<string> = Object.keys(ItemDetails)
 
   return (
     <section id="menu-card">
-      <div className="container">
-        {Object.keys(items).map((category, catIndex) => (
+      <div className="container-fluid">
+        {categories.map((category, catIndex) => (
           <div id={category} className="category-rows" key={catIndex.toString()}>
             <h4>{category}</h4>
             <div className="item-card-box">
               {items[category].map((item, itemIndex) => (
                 <div className="item-card" key={itemIndex.toString()}>
-                  <div className="item-details">
-                    <h6>{item}</h6>
-                  </div>
+                  <h6 className="item-name">
+                    {item.name}
+                  </h6>
+                  <p className="item-desc">
+                    {item.desc}
+                  </p>
+                  <p className="item-price">
+                    {item.price}
+                  </p>
                 </div>
               ))}
             </div>
---
+++ src/components/menu-card/items.js
@@ -0,0 +1,38 @@
+const ItemDetails = {
+  Regular: [
+    {
+      name: 'Chicken Cutlets',
+      desc: 'Minced chicken and potato covered with a breading a fried into a patty',
+      price: 'Rs 180 (6 pieces)',
+    },
+    {
+      name: 'Meat Pepper/Sukka',
+      desc: 'Meat fried with a pepper spiced onion masala',
+      price: 'Rs 470 (500 gms)',
+    },
+    {
+      name: 'Pork Chilly/Bafat',
+      desc: 'Pork cubes stewed with onions and Mangalorean spices',
+      price: 'Rs 380 (500 gms)',
+    },
+    {
+      name: 'Sanna',
+      desc: 'Spongy steamed savoury rice cakes',
+      price: 'Rs 50 (6 pieces)',
+    },
+  ],
+  Specials: [
+    {
+      name: 'Haldi Kholyachyo Patholyo',
+      desc: 'Tumeric leaf dumplings',
+      price: 'Rs 120 (4 pieces)',
+    },
+    {
+      name: 'Mackerel Fry / Masala/ Pulimunchi',
+      desc: 'Fresh Mangalorean Bangudey',
+      price: 'Rs 300 (3 medium sized fish)',
+    },
+
+  ],
+}
+module.exports = ItemDetails
