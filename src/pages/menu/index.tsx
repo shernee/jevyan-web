0d1448949e5dc95a56e121f8fb6c0b5c96debb46
Neetha Sherra <shernee93@gmail.com>
2020-09-11 22:30:34 +0000

    changed path of menu components in main page

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Banner from '../../components/banner'
-import CategoryHeader from '../../components/category-header'
-import MenuCard from '../../components/menu-card'
+import Banner from '../../components/menu/banner'
+import CategoryHeader from '../../components/menu/category-header'
+import MenuCard from '../../components/menu/menu-card'
 import './index.css'
 
 export default function Menu() {
