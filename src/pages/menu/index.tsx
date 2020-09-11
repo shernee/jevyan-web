87ba85b8c20055822ceb2034bf4ed3c6f14c3d64
Neetha Sherra <shernee93@gmail.com>
2020-09-11 19:09:12 +0000

    created below banner section

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import Banner from '../../components/banner'
 import CategoryHeader from '../../components/category-header'
 import MenuCard from '../../components/menu-card'
+import './index.css'
 
 export default function Menu() {
   return (
     <div className="menu-page">
       <Banner />
-      <CategoryHeader />
-      <MenuCard />
+      <div className="below-banner-section">
+        <CategoryHeader />
+        <MenuCard />
+      </div>
     </div>
   )
 }
