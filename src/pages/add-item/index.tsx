fa9d223dfc4d128dd681e6878d08fe389fc3edc9
Neetha Sherra <shernee93@gmail.com>
2020-09-12 07:08:10 +0000

    add-item page

--- src/pages/add-item/index.tsx
+++ src/pages/add-item/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './index.css'
+import AddItemDetails from '../../components/add-item/add-item-details/index'
 
 export default function AddItem() {
   return (
-    <div>
-      Neetha
+    <div className="container-fluid px-0">
+      <div className="add-item-column">
+        <AddItemDetails />
+      </div>
     </div>
   )
 }
