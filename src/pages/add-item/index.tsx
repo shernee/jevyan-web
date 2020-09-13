d1a023d88317f7ac1e00a5f65fadd67ae3e60d9c
Neetha Sherra <shernee93@gmail.com>
2020-09-13 08:09:18 +0000

    added cancel button

--- src/pages/add-item/index.tsx
+++ src/pages/add-item/index.tsx
@@ -1,14 +1,22 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react'
-import { RouteComponentProps } from '@reach/router'
+import { RouteComponentProps, navigate } from '@reach/router'
 import './index.css'
+import CancelOutlinedIcon from '@material-ui/icons/CancelOutlined'
 import AddItemDetails from '../../components/add-item/add-item-details/index'
 import QuantityInput from '../../components/add-item/quantity-plus-minus/index'
 import AddItemButton from '../../components/add-item/add-item-button/index'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function AddItem(props: RouteComponentProps) {
+  const handleCancelPage = () => {
+    navigate('/')
+  }
   return (
     <div className="container-fluid px-0">
+      <div className="cancel-button" role="button" tabIndex={0} onClick={handleCancelPage}>
+        <CancelOutlinedIcon fontSize="large" />
+      </div>
       <div className="add-item-column">
         <AddItemDetails />
         <QuantityInput />
@@ -19,3 +27,4 @@ export default function AddItem(props: RouteComponentProps) {
     </div>
   )
 }
+
