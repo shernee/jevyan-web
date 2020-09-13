28f2ca49a64409a214c645e532e50b5b878d8076
Neetha Sherra <shernee93@gmail.com>
2020-09-13 05:23:34 +0000

    made bottom add item button sticky

--- src/pages/add-item/index.tsx
+++ src/pages/add-item/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './index.css'
 import AddItemDetails from '../../components/add-item/add-item-details/index'
 import QuantityInput from '../../components/add-item/quantity-plus-minus/index'
+import AddItemButton from '../../components/add-item/add-item-button/index'
 
 export default function AddItem() {
   return (
@@ -9,6 +10,9 @@ export default function AddItem() {
       <div className="add-item-column">
         <AddItemDetails />
         <QuantityInput />
+      </div>
+      <div className="bottom-sticky-button">
+        <AddItemButton />
       </div>
     </div>
   )
