ce8065d8dbdc3bcef3008e5ab79bb09f2a166ffe
Neetha Sherra <shernee93@gmail.com>
2020-09-13 05:23:09 +0000

    bottom add item button

---
+++ src/components/add-item/add-item-button/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.css'
+
+export default function AddItemButton() {
+  return (
+    <button type="button" className="btn btn-dark btn-block rounded-0">
+      Add Item to Order
+    </button>
+  )
+}
