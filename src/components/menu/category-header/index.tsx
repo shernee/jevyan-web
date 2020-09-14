ff14f8ae955a004244b320863b14ede5e94775aa
Neetha Sherra <shernee93@gmail.com>
2020-09-14 06:54:32 +0000

    category navbar uses json for values

--- src/components/menu/category-header/index.tsx
+++ src/components/menu/category-header/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './index.css'
+import ItemDetails from '../menu-card/items'
 
 export default function CategoryHeader() {
+  const categories: Array<string> = Object.keys(ItemDetails)
   return (
     <nav className="navbar px-0 sticky-top navbar-expand navbar-custom">
       <ul className="nav navbar-nav">
-        <li><a href="#Regular">Regular</a></li>
-        <li><a href="#Specials">Specials</a></li>
+        {categories.map((category, catIndex) => (
+          <li key={catIndex.toString()}><a href={`#${category}`}>{category}</a></li>
+        ))}
       </ul>
     </nav>
   )
