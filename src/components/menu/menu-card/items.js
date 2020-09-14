a1850046f1184efecfcf46434eec1f0e678622cb
Neetha Sherra <shernee93@gmail.com>
2020-09-14 05:51:46 +0000

    added item id's

--- src/components/menu/menu-card/items.js
+++ src/components/menu/menu-card/items.js
@@ -1,24 +1,28 @@
 const ItemDetails = {
   Regular: [
     {
+      id: 1,
       name: 'Chicken Cutlets',
       desc: 'Minced chicken and potato covered with a breading a fried into a patty',
       price: 'Rs 180 (6 pieces)',
       image: '/images/chicken-cutlet.png',
     },
     {
+      id: 2,
       name: 'Meat Pepper/Sukka',
       desc: 'Meat fried with a pepper spiced onion masala',
       price: 'Rs 470 (500 gms)',
       image: '/images/meat-pepper-sukka.png',
     },
     {
+      id: 3,
       name: 'Pork Chilly/Bafat',
       desc: 'Pork cubes stewed with onions and Mangalorean spices',
       price: 'Rs 380 (500 gms)',
       image: '/images/pork-chilly-bafat.png',
     },
     {
+      id: 4,
       name: 'Sanna',
       desc: 'Spongy steamed savoury rice cakes',
       price: 'Rs 50 (6 pieces)',
@@ -27,12 +31,14 @@ const ItemDetails = {
   ],
   Specials: [
     {
+      id: 5,
       name: 'Haldi Kholyachyo Patholyo',
       desc: 'Tumeric leaf dumplings',
       price: 'Rs 120 (4 pieces)',
       image: '/images/patholyo.png',
     },
     {
+      id: 6,
       name: 'Mackerel Fry / Masala/ Pulimunchi',
       desc: 'Fresh Mangalorean Bangudey',
       price: 'Rs 300 (3 medium sized fish)',
