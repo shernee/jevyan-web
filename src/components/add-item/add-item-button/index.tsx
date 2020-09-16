3f75831db411b6b1b614191436546435401bcd66
Neetha Sherra <shernee93@gmail.com>
2020-09-16 05:25:11 +0000

    get add item button to the center in desktop

--- src/components/add-item/add-item-button/index.tsx
+++ src/components/add-item/add-item-button/index.tsx
@@ -3,7 +3,7 @@ import './index.css'
 
 export default function AddItemButton() {
   return (
-    <button type="button" className="btn btn-dark btn-block rounded-0">
+    <button type="button" className="btn btn-dark rounded-0">
       Add Item to Order
     </button>
   )
