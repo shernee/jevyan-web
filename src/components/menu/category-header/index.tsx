9208277330c5b232e3485067fe9b41b8dd6f425b
Neetha Sherra <shernee93@gmail.com>
2020-09-16 19:35:48 +0000

    create cart link on menu page

--- src/components/menu/category-header/index.tsx
+++ src/components/menu/category-header/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from '@reach/router'
+import ShoppingBasketOutlinedIcon from '@material-ui/icons/ShoppingBasketOutlined'
 import './index.css'
 import ItemDetails from '../menu-card/items'
 
@@ -6,11 +8,18 @@ export default function CategoryHeader() {
   const categories: Array<string> = Object.keys(ItemDetails)
   return (
     <nav className="navbar px-0 sticky-top navbar-expand navbar-custom">
-      <ul className="nav navbar-nav">
-        {categories.map((category, catIndex) => (
-          <li key={catIndex.toString()}><a href={`#${category}`}>{category}</a></li>
-        ))}
-      </ul>
+      <div className="container-fluid px-0">
+        <ul className="nav navbar-nav">
+          {categories.map((category, catIndex) => (
+            <li key={catIndex.toString()}><a href={`#${category}`}>{category}</a></li>
+          ))}
+        </ul>
+        <div className="cart-link">
+          <Link to="/cart">
+            <ShoppingBasketOutlinedIcon fontSize="large" style={{ color: 'red' }} />
+          </Link>
+        </div>
+      </div>
     </nav>
   )
 }
