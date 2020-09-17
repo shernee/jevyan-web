bc6aaac22af8d15b72f05586a7d942153d69e229
Neetha Sherra <shernee93@gmail.com>
2020-09-17 02:21:16 +0000

    added cart link and cart quantity to menu navbar

--- src/components/menu/category-header/index.tsx
+++ src/components/menu/category-header/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Link } from '@reach/router'
-import ShoppingBasketOutlinedIcon from '@material-ui/icons/ShoppingBasketOutlined'
+import RestaurantMenuIcon from '@material-ui/icons/RestaurantMenu'
 import './index.css'
 import ItemDetails from '../menu-card/items'
 
+interface IDict {
+  id: string,
+  quantity: string,
+}
+
 export default function CategoryHeader() {
   const categories: Array<string> = Object.keys(ItemDetails)
+  let localCart: any = localStorage.getItem('cart')
+  localCart = JSON.parse(localCart)
+  let total: number = 0
+  if (localCart) total = localCart.reduce((prev: IDict, next: IDict) => prev + next.quantity, 0)
   return (
     <nav className="navbar px-0 sticky-top navbar-expand navbar-custom">
       <div className="container-fluid px-0">
@@ -16,7 +25,10 @@ export default function CategoryHeader() {
         </ul>
         <div className="cart-link">
           <Link to="/cart">
-            <ShoppingBasketOutlinedIcon fontSize="large" style={{ color: 'red' }} />
+            <RestaurantMenuIcon style={{ fontSize: 32, color: '#9B26B6', verticalAlign: 'top' }} />
+            <span className="cart-total">
+              {total}
+            </span>
           </Link>
         </div>
       </div>
