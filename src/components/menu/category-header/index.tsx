f6b42469f077a801bdf3766ff9029e4b760b2998
Neetha Sherra <shernee93@gmail.com>
2020-09-21 00:27:06 +0000

    category header using api data

--- src/components/menu/category-header/index.tsx
+++ src/components/menu/category-header/index.tsx
@@ -1,40 +1,23 @@
 import React from 'react'
-import { Link } from '@reach/router'
-import RestaurantMenuIcon from '@material-ui/icons/RestaurantMenu'
 import './index.css'
-import ItemDetails from '../menu-card/items'
+import { categoryShape } from '../../../data/type'
 
-interface CategoryProps {
-  cartTotal: number;
+interface ICategoryProps {
+  categories: Array<categoryShape>;
 }
 
-const CategoryHeader = (props: CategoryProps) => {
+export default function CategoryHeader(props: ICategoryProps) {
   const {
-    cartTotal,
+    categories,
   } = props
-  const categories: Array<string> = Object.keys(ItemDetails)
-  const screenWidth = window.innerWidth
+
   return (
     <nav className="navbar px-0 sticky-top navbar-expand navbar-custom">
-      <div className="container-fluid px-0">
-        <ul className="nav navbar-nav">
-          {categories.map((category, catIndex) => (
-            <li key={catIndex.toString()}><a href={`#${category}`}>{category}</a></li>
-          ))}
-        </ul>
-        {(screenWidth > 768) && (
-          <div className="cart-link">
-            <Link to="/cart">
-              <RestaurantMenuIcon style={{ fontSize: 32, color: '#9B26B6', verticalAlign: 'top' }} />
-              <span className="cart-total">
-                {cartTotal}
-              </span>
-            </Link>
-          </div>
-        )}
-      </div>
+      <ul className="nav navbar-nav">
+        {categories.map((category) => (
+          <li key={category.id}><a href={`#${category.name}`}>{category.name}</a></li>
+        ))}
+      </ul>
     </nav>
   )
 }
-
-export default CategoryHeader
