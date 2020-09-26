ed04212a73774c2c2cbf14cb0e83f55a41d6b151
Neetha Sherra <shernee93@gmail.com>
2020-09-26 06:05:34 +0000

    cart icon shifted to right

--- src/components/menu/category-header/index.tsx
+++ src/components/menu/category-header/index.tsx
@@ -17,21 +17,23 @@ export default function CategoryHeader(props: ICategoryProps) {
 
   return (
     <nav className="navbar px-0 sticky-top navbar-expand navbar-custom">
-      <ul className="nav navbar-nav">
-        {categories.map((category) => (
-          <li key={category.id}><a href={`#${category.name}`}>{category.name}</a></li>
-        ))}
-      </ul>
-      {(screenWidth > 768) && (
-        <div className="cart-link">
-          <Link to="/cart">
-            <RestaurantMenuIcon style={{ fontSize: 32, color: '#9B26B6', verticalAlign: 'top' }} />
-            <span className="cart-total">
-              {cartQuantity}
-            </span>
-          </Link>
-        </div>
-      )}
+      <div className="container-fluid px-0">
+        <ul className="nav navbar-nav">
+          {categories.map((category) => (
+            <li key={category.id}><a href={`#${category.name}`}>{category.name}</a></li>
+          ))}
+        </ul>
+        {(screenWidth > 768) && (
+          <div className="cart-link">
+            <Link to="/cart">
+              <RestaurantMenuIcon style={{ fontSize: 32, color: '#9B26B6', verticalAlign: 'top' }} />
+              <span className="cart-total">
+                {cartQuantity}
+              </span>
+            </Link>
+          </div>
+        )}
+      </div>
     </nav>
   )
 }
