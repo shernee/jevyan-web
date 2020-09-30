c9cca3df1877006b98b9fa9ca5fdff156d7523df
Neetha Sherra <shernee93@gmail.com>
2020-09-30 05:14:04 +0000

    dynamic document title for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import { Router } from '@reach/router'
 import Menu from './pages/menu'
 import AddItem from './pages/add-item'
@@ -6,6 +7,15 @@ import Cart from './pages/cart'
 import './App.css'
 
 function App() {
+  React.useEffect(() => {
+    const subDomain = window.location.hostname.split('.')[0]
+    const bannerUrl = `${window.location.origin}/api/org/banner/${subDomain}/`
+    const loadData = async () => {
+      const bannerResponse = await axios.get(bannerUrl)
+      document.title = bannerResponse.data.name
+    }
+    loadData()
+  }, [])
   return (
     <div className="App">
       <Router basepath="/">
