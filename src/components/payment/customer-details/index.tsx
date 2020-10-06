177276e7433a8b220e14b4290969fdf80e7cdc5a
Neetha Sherra <shernee93@gmail.com>
2020-10-06 19:18:42 +0000

    payment page delivery details section

--- src/components/payment/customer-details/index.tsx
+++ src/components/payment/customer-details/index.tsx
@@ -1,17 +1,62 @@
 import React from 'react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 import TextField from '@material-ui/core/TextField'
+import { deliveryFromStorage } from '../../../helper/helper'
+import { deliveryShape } from '../../../data/type'
 import './index.css'
 
 const CustomerDetails = () => {
-  const fields = ['name', 'email', 'phone', 'address']
-  const labels = ['Name', 'Email address', 'Phone number', 'Address']
-  const initValues = ['', '', '', '']
-  const initState = fields.map((f, i) => ({
-    field: f,
-    label: labels[i],
-    value: initValues[i],
-  }))
-  const [Details, setDetails] = React.useState(initState)
+  const localDelivery: deliveryShape = deliveryFromStorage()
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+      street: '',
+      landmark: '',
+      neighborhood: '',
+      city: '',
+      state: '',
+      day: localDelivery.day,
+      time: localDelivery.time,
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .max(32, '32 characters or less')
+        .required('Required'),
+      lastName: Yup.string()
+        .max(32, '32 characters or less')
+        .required('Required'),
+      email: Yup.string()
+        .email()
+        .max(64, '64 characters or less')
+        .required('Required'),
+      phone: Yup.string()
+        .matches(/^[0-9]{10}$/, 'Must be a 10 digit number')
+        .max(10, '10 digits or less')
+        .required('Required'),
+      street: Yup.string()
+        .max(64, '64 characters or less')
+        .required('Required'),
+      landmark: Yup.string()
+        .max(64, '64 characters or less'),
+      neighborhood: Yup.string()
+        .max(32, '32 characters or less')
+        .required('Required'),
+      city: Yup.string()
+        .max(32, '32 characters or less')
+        .required('Required'),
+      state: Yup.string()
+        .max(32, '32 characters or less')
+        .required('Required'),
+    }),
+    onSubmit: (values) => {
+      // eslint-disable-next-line no-alert
+      alert(JSON.stringify(values, null, 2))
+    },
+  })
 
   return (
     <div className="customer-details-section">
@@ -20,18 +65,166 @@ const CustomerDetails = () => {
           Delivery Details
         </h5>
       </div>
-      {Details.map((detail, index) => (
-        <div className="detail-input">
+      <form onSubmit={formik.handleSubmit}>
+        <div className="detail-fields detail-name">
           <TextField
-            key={index.toString()}
-            id="standard-basic"
-            label={detail.label}
+            name="firstName"
+            id="firstName"
+            label="First name"
             placeholder=""
             multiline
-            value={detail.value}
+            type="text"
+            variant="outlined"
+            helperText={formik.errors.firstName && formik.touched.firstName ? formik.errors.firstName : ''}
+            value={formik.values.firstName}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          <TextField
+            name="lastName"
+            id="lastName"
+            label="Last Name"
+            placeholder=""
+            multiline
+            type="text"
+            variant="outlined"
+            helperText={formik.errors.lastName && formik.touched.lastName ? formik.errors.lastName : ''}
+            value={formik.values.lastName}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           />
         </div>
-      ))}
+        <div className="detail-fields detail-email-phone">
+          <TextField
+            name="email"
+            id="email"
+            label="Email address"
+            placeholder="Valid email address"
+            multiline
+            type="email"
+            variant="outlined"
+            helperText={formik.errors.email && formik.touched.email ? formik.errors.email : ''}
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          <TextField
+            name="phone"
+            id="phone"
+            label="Contact number"
+            placeholder="10 digit contact number"
+            multiline
+            type="text"
+            variant="outlined"
+            helperText={formik.errors.phone && formik.touched.phone ? formik.errors.phone : ''}
+            value={formik.values.phone}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+        </div>
+        <div className="detail-fields detail-address-firstline">
+          <TextField
+            name="street"
+            id="street"
+            label="Street"
+            placeholder=""
+            multiline
+            type="string"
+            variant="outlined"
+            helperText={formik.errors.street && formik.touched.street ? formik.errors.street : ''}
+            value={formik.values.street}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          <TextField
+            name="landmark"
+            id="landmark"
+            label="Landmark"
+            placeholder="Ex: Behind Big bazaar"
+            multiline
+            type="string"
+            variant="outlined"
+            helperText={formik.errors.landmark && formik.touched.landmark ? formik.errors.landmark : ''}
+            value={formik.values.landmark}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+        </div>
+        <div className="detail-fields detail-address-secondline">
+          <TextField
+            name="neighborhood"
+            id="neighborhood"
+            label="Neighborhood"
+            placeholder="Ex: Koramangala"
+            multiline
+            type="string"
+            variant="outlined"
+            helperText={formik.errors.neighborhood && formik.touched.neighborhood ? formik.errors.neighborhood : ''}
+            value={formik.values.neighborhood}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          <TextField
+            name="city"
+            id="city"
+            label="City"
+            placeholder=""
+            multiline
+            type="string"
+            variant="outlined"
+            helperText={formik.errors.city && formik.touched.city ? formik.errors.city : ''}
+            value={formik.values.city}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          <TextField
+            name="state"
+            id="state"
+            label="State"
+            placeholder=""
+            multiline
+            type="string"
+            variant="outlined"
+            helperText={formik.errors.state && formik.touched.state ? formik.errors.state : ''}
+            value={formik.values.state}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+        </div>
+        <div className="detail-fields detail-delivery-time">
+          <TextField
+            name="day"
+            id="day"
+            label="Delivery day"
+            placeholder=""
+            multiline
+            type="string"
+            inputProps={{
+              readOnly: true,
+            }}
+            variant="outlined"
+            value={formik.values.day}
+          />
+          <TextField
+            name="time"
+            id="time"
+            label="Delivery time"
+            placeholder=""
+            multiline
+            type="string"
+            inputProps={{
+              readOnly: true,
+            }}
+            variant="outlined"
+            value={formik.values.time}
+          />
+        </div>
+        <div className="detail-submit">
+          <button type="submit" className="btn btn-dark rounded-0">
+            Save
+          </button>
+        </div>
+      </form>
     </div>
   )
 }
